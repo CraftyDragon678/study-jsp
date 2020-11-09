3c3ed2f34d2e24da12fa4afbb2203cf9bf743e74
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-09 13:57:55 +0900

    first servlet

---
+++ src/com/cragon/HelloServlet.java
@@ -0,0 +1,14 @@
+package com.cragon;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "HelloServlet")
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
