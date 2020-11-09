68148c68a193032cf16e5d1f47e4aaff7e9eaa41
CraftyDragon678 <craftydragon678@gmail.com>
2020-11-09 17:41:24 +0900

    hello servlet

--- src/com/cragon/HelloServlet.java
+++ src/com/cragon/HelloServlet.java
@@ -1,14 +1,22 @@
 package com.cragon;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@javax.servlet.annotation.WebServlet(name = "HelloServlet")
+@javax.servlet.annotation.WebServlet("/HelloServlet")
 public class HelloServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        PrintWriter out = response.getWriter();
+        out.write("<html>");
+        out.write("<head>");
+        out.write("</head>");
+        out.write("<body>");
+        out.write("hello!!");
+        out.write("</body>");
+        out.write("</html>");
     }
 }
